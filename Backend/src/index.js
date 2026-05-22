a8abd35c4a4bb4a6e2a2434af9a80e3ca3f6427b
Agustin <agustin.oleinizak3@gmail.com>
2026-05-21 23:05:36 -0300

    add files

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { connectDB } from "./config/db.js"; 
+import cookieParser from "cookie-parser"
+import morgan from "morgan";
+import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
+import taskRoutes from "./routes/tasks.routes.js";
 
 dotenv.config();
 connectDB();
@@ -9,11 +13,13 @@ connectDB();
 const app = express();
 const PORT = 5000
 
-
+app.use(morgan('dev'));
 app.use(cors());
 
 app.use(express.json());
-
+app.use(cookieParser());
+app.use("/api", authRoutes);
+app.use("/api", taskRoutes);
 
 app.get('/', (req, res) => {
     res.send('¡Hola mundo con Node.js y Express!');
